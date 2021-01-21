35e38b1c1d14bf89cdced2469f3ea4ac35df5eb4
ics-nishihara <nishihara@ics-web.jp>
2021-01-21 21:13:09 +0900

    faviconのパスを修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,6 +10,7 @@ export default {
       { name: "viewport", content: "width=device-width, initial-scale=1" },
       { hid: "description", name: "description", content: "" },
     ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/211020_nuxt_composition_api_full_static/favicon.ico" }],
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
