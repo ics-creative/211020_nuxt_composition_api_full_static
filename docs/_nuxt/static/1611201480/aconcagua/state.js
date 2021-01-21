185a7f8d43c989c8f2211e376e353a6be0f169e4
ics-nishihara <nishihara@ics-web.jp>
2021-01-21 12:58:38 +0900

    トレイリングスラッシュの設定変更

---
+++ docs/_nuxt/static/1611201480/aconcagua/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002F211020_nuxt_composition_api_full_static\u002F_nuxt\u002Fstatic\u002F1611201480",layout:"default",error:null,serverRendered:true,routePath:"\u002Faconcagua",config:{http:{browserBaseURL:"\u002F"}},globalRefs:{}};
